068918fb06adff46de24be81f45fb0b5b11a5b38
Michal Gajda <michal@gajda.co.uk>
2025-01-02 15:35:41 +0000

    Add OpenTelemetry

--- src/RanTanPlan/Program.cs
+++ src/RanTanPlan/Program.cs
@@ -1,12 +1,33 @@
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+const string OTLP_LOGS = "http://localhost:5341/ingest/otlp/v1/logs";
+const string OTLP_TRACES = "http://localhost:5341/ingest/otlp/v1/traces";
+const string SERVICE_NAME = "RanTanPlan";
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(SERVICE_NAME)).AddOtlpExporter(configure => { configure.Endpoint = new Uri(OTLP_LOGS); configure.Protocol = OtlpExportProtocol.HttpProtobuf; });
+});
+
+builder.Services.AddOpenTelemetry()
+      .ConfigureResource(resource => resource.AddService(SERVICE_NAME))
+      .WithTracing(tracing => tracing
+          .AddAspNetCoreInstrumentation()
+          .AddOtlpExporter(configure => { configure.Endpoint = new Uri(OTLP_TRACES); configure.Protocol = OtlpExportProtocol.HttpProtobuf; }))
+      .WithMetrics(metrics => metrics
+          .AddAspNetCoreInstrumentation()
+          .AddOtlpExporter());
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -19,7 +40,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
--- src/RinTinTin/Program.cs
+++ src/RinTinTin/Program.cs
@@ -1,9 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using Refit;
 using RinTinTin;
 using RinTinTin.Interfaces;
 
+const string OTLP_LOGS = "http://localhost:5341/ingest/otlp/v1/logs";
+const string OTLP_TRACES = "http://localhost:5341/ingest/otlp/v1/traces";
+const string SERVICE_NAME = "RinTinTin";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(SERVICE_NAME)).AddOtlpExporter(configure => { configure.Endpoint = new Uri(OTLP_LOGS); configure.Protocol = OtlpExportProtocol.HttpProtobuf; });
+});
+
+builder.Services.AddOpenTelemetry()
+      .ConfigureResource(resource => resource.AddService(SERVICE_NAME))
+      .WithTracing(tracing => tracing
+          .AddAspNetCoreInstrumentation()
+          .AddHttpClientInstrumentation()
+          .AddOtlpExporter(configure => { configure.Endpoint = new Uri(OTLP_TRACES); configure.Protocol = OtlpExportProtocol.HttpProtobuf; }))
+      .WithMetrics(metrics => metrics
+          .AddAspNetCoreInstrumentation()
+          .AddOtlpExporter());
 
 builder.Services.AddSingleton(builder.Configuration.GetSection("RanTanPlan").Get<RanTanPlanOptions>()!);
 
